76a1aa85a505aed0ce872acf2d12d80142cf60a9
saber2pr <saber2pr@gmail.com>
2021-09-19 11:13:21 +0800

    chore: update

--- src/test.ts
+++ src/test.ts
@@ -2,7 +2,13 @@ import { parse } from '.'
 
 const code = `
 <div id="233" class="qwq">
-  <span>"aaa"</span>
+  <span>aaa</span>
+  <span>1234</span>
+  <span>1234asd</span>
+  <span>
+    12
+    aa
+  </span>
 </div>
 `
 console.log(JSON.stringify(parse(code), null, 2))
